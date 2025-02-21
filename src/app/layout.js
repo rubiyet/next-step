3238a5391fcd21dbeef72ffb8b9f6d64004fc774
rubiyet <rubiyet.fardous@gmail.com>
2025-02-22 04:05:15 +0600

    metadata fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,13 +13,28 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://next-step-3.vercel.app"),
   title: "Next Step",
   description: "Developed by @Rubiyet",
-  image: "/rubiyet.jpeg",
-  url: "https://next-step.vercel.app",
-  type: "website",
   keywords: "next.js, tailwindcss, shadcdn",
   siteName: "Next Step ♾️",
+
+  openGraph: {
+    title: "Next Step",
+    description: "Developed by @Rubiyet",
+    url: "https://next-step-3.vercel.app",
+    type: "website",
+    siteName: "Next Step ♾️",
+    images: [
+      {
+        url: "/rubiyet.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Next Step",
+      },
+    ],
+  },
+  
 };
 
 export default function RootLayout({ children }) {
